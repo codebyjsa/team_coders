2b984a12551e5233384a4136e3699c2722e9fe1b
codebyjsa <jasishere@gmail.com>
2025-02-04 22:03:49 +0530

    new commit

--- Jasdeep/Primary/JAVA/Frameworks/Spring Boot/02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/DemoController.java
+++ Jasdeep/Primary/JAVA/Frameworks/Spring Boot/02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/DemoController.java
@@ -1,7 +1,5 @@
 package com.luv2code.springcoredemo.common;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,22 +11,13 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController (@Qualifier("cricketCoach") Coach theCoach){
+    public DemoController (@Qualifier("aquatic") Coach theCoach){
         System.out.println("In Constructor: "+getClass().getSimpleName());
         myCoach = theCoach;
     }
 
     // define our init method
-    @PostConstruct
-    public void doMyStartupStuff(){
-        System.out.println("In doMyStartupStuff(): "+ getClass().getSimpleName());
-    }
 
-    //define our destroy method
-    @PreDestroy
-    public void doMyCleanupStuff(){
-        System.out.println("In doMyCleanupStuff(): "+ getClass().getSimpleName());
-    }
 
     @GetMapping("/dailyworkout")
     public  String getDailyWorkout(){
