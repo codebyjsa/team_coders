affd136f51561c0ec43521554558ee2557e9fec5
Jashancoder12 <jashansandhu2206@gmail.com>
2025-08-09 11:48:44 +0530

    javascript arrays and functions

---
+++ Jashanpreet/Primary/Web Dev/function1.js
@@ -0,0 +1,14 @@
+
+//functions
+ function myfunction(n1,n2){
+    console.log("multipliation of two numbers is: " ,n1*n2)
+}
+myfunction(12,2)  
+
+
+// Arrow Function
+const arrowsum = (a,b) => {
+    console.log(" addition of two numbers is: ",a+b)
+
+}
+arrowsum(1,4)
