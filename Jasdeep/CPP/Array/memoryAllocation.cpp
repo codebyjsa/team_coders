1d710b7f9c0f42579ddce436fadb6f4e661091c2
codebyjsa <jsaishere@gmail.com>
2025-08-13 16:27:57 +0530

    CPP Learnt

---
+++ Jasdeep/CPP/Array/memoryAllocation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10]; // Declare an array of size 10
+    cout<<&arr[0]<<endl; // Print the address of the first element of the array
+    cout<<&arr[1]<<endl; // Print the address of the second element of the array
+    cout<<&arr[2]<<endl; // Print the address of the third element of the array
+    cout<<&arr[3]<<endl; // Print the address of the fourth element of the array
+    cout<<&arr[4]<<endl; // Print the address of the fifth element of the array
+    cout<<&arr[5]<<endl; //
+    return 0;
+}
